2d1b9c98860950723f609bebdd94d969eb261935
Jonas Sharron desktop 202 <jfsharron@msn.com>
2021-01-23 17:59:06 -0800

    added documentation to q1 Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit. On branch master Changes to be committed: deleted:    a.exe modified:   source.cpp Untracked files: .history/source_20210122154850.cpp .history/source_20210122155308.cpp .history/source_20210123175449.cpp

--- q1/source.cpp
+++ q1/source.cpp
@@ -1,3 +1,14 @@
+//  ============================================================================
+//  Program:               QuizOneProject
+//  Software Engineer:     Jonas Sharron
+//  Original Date:         25 September 2017
+//  Revised Date:          22 January 2021
+//  Purpose:               Given the input of two integers from the user, this 
+//                         program will output either the sum, product, and
+//                         difference of the integers or the value of raising 
+//                         one integer to power of the other.
+//  ============================================================================
+
 #include <iostream>
 using namespace std;
 
@@ -10,20 +21,23 @@ int power(int, int);
 
 int main(){
 
-    int x;
-    int y;
-    int sum;
-    int product;
-    int difference;
-    int menu;
-    int menu2;
-    bool goAgain;
-    bool goAgainNew = true;
+    int x;                          //  first integer
+    int y;                          //  second integer
+    int sum;                        //  variable for holding sum
+    int product;                    //  variable for holding product
+    int difference;                 //  variable for holding difference
+    int menu;                       //  variable for user input in menu
+    int menu2;                      //  variable for user input in menu2
+    bool goAgain;                   //  flag to determine loop with same values
+    bool goAgainNew = true;         //  flag to determine loop with new values
 
+    //  banner
     cout << endl;
     cout << "This program will compute the sum, product, difference," << endl;
     cout << "and power of two user-supplied integers." << endl;
 
+    // user input and entry point of initial loop (as well as entry point if
+    //  user chooses to run program again with new values)
     while(goAgainNew == true){
         cout << endl;
         cout << "Please enter first integer: ";
@@ -32,6 +46,8 @@ int main(){
         cin >> y;
         goAgain = true;
 
+        //  user input for what to compute (also entry point of loop if user 
+        //  chooses to re-run program with the same values)
         while (goAgain == true){
             cout << endl;
             cout << "Compute the sum, product difference -or- power?" << endl;
@@ -41,6 +57,8 @@ int main(){
             cin >> menu;
             cout << endl;
 
+            //  switch statement to direct program flow based on user choice of
+            //  what to compute above
             switch (menu){
                 case 1:
                 sumProductDifference(x, y, sum, product, difference);
@@ -54,6 +72,8 @@ int main(){
                 break;
             } // end menu switch
 
+            //  user input on how to continue after computations and respective
+            //  switch statement
             cout << endl;
             cout << "what would you like to do next?" << endl;
             cout << "1 - go again with same integers" << endl;
@@ -79,6 +99,18 @@ int main(){
     }  // end while(goAgainNew) loop
 }//  end main
 //  ============================================================================
+//  Function:        sumProductDifference
+//  Purpose:         calculate the sum, product, and difference of two integers
+//  Parameters:      int x - integer one for calculation
+//                   int y - integer two for calculation
+//                   int& sum - value of sum (of x and y) reference (for return)
+//                   int& product - value of product (of x and y) reference (for
+//                                  return)
+//                   int& difference - value of difference (of x and y) reference
+//                                     (for return)
+//  Returns:         nothing (void function, calculated vales - sum, product,
+//                   difference are passed by reference)
+//  ============================================================================
 void sumProductDifference(int x, int y, int& sum, int& product, int& difference){
 
     sum = x + y;
@@ -87,6 +119,11 @@ void sumProductDifference(int x, int y, int& sum, int& product, int& difference)
 
 }  //end sumProductDifference
 //  ============================================================================  
+//  Function:   power
+//  Purpose:    calculate the value of one integer raised to the power of another
+//  Parameters: int x - the base integer
+//              int y - the exponent integer
+//  Returns:    power - the value of int x raised to the power of int y  
 int power(int x, int y){
 
     int power = x;
